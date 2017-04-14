365b504c06d9b5567e46c51be465d169b1c543cc
Rahul <libra.rahul.sadhukhan@gmail.com>
2017-04-14 08:24:08 +0530

    Removed carousal and added Media Carousal

--- virtual-trainer/client/views/dashboard.js
+++ virtual-trainer/client/views/dashboard.js
@@ -1,6 +1,5 @@
 import './dashboard.html';
 
 Template.dashboard.onRendered(function() {
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-  $('.carousel').carousel('next');
+  $('.slider').slider();
 });
